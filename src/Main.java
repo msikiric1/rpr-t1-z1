b2cff4c702733681399b48e84e847459aac61daf
msikiric1 <msikiric1@etf.unsa.ba>
2022-10-29 18:41:18 +0200

    added main functionality

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+package ba.unsa.etf.rpr;
+
+/**
+ * @author Muaz Sikiric
+ * @version 1.0
+ */
 public class Main {
+    /**
+     * @param args input arguments from console
+     */
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner in = new Scanner(System.in);
+        System.out.println("Unesite x: ");
+        int x = in.nextInt();
+        System.out.println("Unesite y: ");
+        int y = in.nextInt();
+        System.out.println("x: " + x + ", y: " + y);
     }
 }
