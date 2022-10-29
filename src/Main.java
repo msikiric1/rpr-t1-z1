3ca2d6e342b2dbe63bf75b3f0c066c84141963ba
msikiric1 <msikiric1@etf.unsa.ba>
2022-10-29 18:51:32 +0200

    fixed some bugs

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,4 @@
-package ba.unsa.etf.rpr;
-
-/**
- * @author Muaz Sikiric
- * @version 1.0
- */
 public class Main {
-    /**
-     * @param args input arguments from console
-     */
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Unesite x: ");
-        int x = in.nextInt();
-        System.out.println("Unesite y: ");
-        int y = in.nextInt();
-        System.out.println("x: " + x + ", y: " + y);
     }
 }
--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -1,13 +1,22 @@
 package ba.unsa.etf.rpr;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    /**
+     * @param args input arguments from console
+     */
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Unesite x: ");
+        int x = in.nextInt();
+        System.out.println("Unesite y: ");
+        int y = in.nextInt();
+        System.out.println("x: " + x + ", y: " + y);
     }
 }
